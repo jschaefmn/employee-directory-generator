90c428248ba0c462fdde9acd6aec2eb15b1f10bc
Jake Schaefbauer <jschaefmn@gmail.com>
2022-07-02 08:24:04 -0500

    employee constructor passes all tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,3 +8,32 @@ test('creates a employee object', () => {
   expect(employee.id).toEqual(expect.any(Number));
   expect(employee.email).toEqual(expect.any(String));
 });
+
+// get name from getName
+test('gets employee name', () => {
+  const employee = new Employee('Jake', 23, 'jschaefmn@gmail.com');
+
+  expect(employee.getName()).toEqual(expect.any(String));
+});
+
+// get id from getId
+test('get employee id', () => {
+  const employee = new Employee('Jake', 23, 'jschaefmn@gmail.com');
+
+  expect(employee.getId()).toEqual(expect.any(Number));
+})
+
+// get employee email from getEmail
+test('get employee email', () => {
+  const employee = new Employee('Jake', 23, 'jschaefmn@gmail.com');
+
+  expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+// get employee role from getRole()
+test('get employee role', () => {
+  const employee = new Employee('Jake', 23, 'jschaefmn@gmail.com');
+
+  expect(employee.getRole()).toEqual("Employee");
+})
+
