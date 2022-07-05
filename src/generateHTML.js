a4f9814a73b5013fdb38758601188d7057c1eeee
Jake Schaefbauer <jschaefmn@gmail.com>
2022-07-04 20:10:38 -0500

    final commit

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -31,7 +31,7 @@ const generateEngineer = function (engineer) {
         <div class="card-body">
           <p class="id">ID: ${engineer.id}</p>
           <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
-          <p class="github">GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+          <p class="github">GitHub: <a href="https://github.com/${engineer.github}" target="_blank">${engineer.github}</a></p>
         </div>
       </div>
     </div>
@@ -41,7 +41,7 @@ const generateEngineer = function (engineer) {
 // intern card
 const generateIntern = function (intern) {
   return `
-  <div class="col-4 mt-4>
+  <div class="col-4 mt-4">
     <div class="card h-100">
       <div class="card-header">
         <h3>${intern.name}</h3>
@@ -53,7 +53,7 @@ const generateIntern = function (intern) {
         <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
         <p class="school">School: ${intern.school}</p>
       </div>
-    </div
+    </div>
   </div>
   `
 };
